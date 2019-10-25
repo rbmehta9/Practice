a4e46d54c5b1e7a5e56827a7cf62bfb033037884
Ritesh Mehta <Ritesh.Mehta@csgi.com>
2019-10-24 21:36:16 -0500

    commit

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -33,6 +33,95 @@ namespace Trees
             InOrder(node.right);
         }
 
+        static void PreOrder(Node node)
+        {
+            if (node == null)
+                return;
+
+            Console.WriteLine($"Value is {node.Value}");
+            PreOrder(node.left);
+            PreOrder(node.right);
+        }
+
+        static void PreOrderWithoutRecursion()
+        {
+            var node = root;
+            while (node != null || stack.Any())
+            {
+                
+                //TraverseLeftOnlyAndPushOnStack(node);
+                var currentNode = node;
+                while (currentNode != null)
+                {
+                    Console.WriteLine($"{currentNode.Value}");
+                    stack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+                node = stack.Peek();
+                stack.Pop();
+                
+                node = node.right;
+            }
+        }
+
+        static void PostOrder(Node node)
+        {
+            if (node == null)
+                return;
+
+            PostOrder(node.left);
+            PostOrder(node.right);
+            Console.WriteLine($"Value is {node.Value}");
+        }
+
+        static void PostOrderWithoutRecursion()
+        {
+            var currentNode = root;
+            Node lastNode = null;
+            while (true)
+            {
+                while (currentNode!=null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+
+                currentNode = stack.Peek();
+
+                if (currentNode != null && currentNode.right != null)
+                    currentNode = currentNode.right;
+                else
+                {
+                    Node prevNode = null;
+                    while (true)
+                    {
+                        lastNode = stack.Peek();
+                        if (lastNode.right == null || lastNode.right == prevNode)
+                        {
+                            Console.WriteLine($"Value is {lastNode.Value}");
+                            stack.Pop();
+                            prevNode = lastNode;
+                            if(!stack.Any())
+                                break;
+                        }
+                        else
+                        {
+                            currentNode = lastNode.right;
+                            break;
+                        }
+                    }
+
+                    if(prevNode == root)
+                        break;
+
+                    
+                }
+
+            }
+
+            
+        }
+
         static void InOrderWithoutRecursion()
         {
             var node = root;
@@ -47,6 +136,19 @@ namespace Trees
                     TraverseLeftOnlyAndPushOnStack(currentStackELement.right);
 
             } while (stack.Count > 0 || currentStackELement.right != null);
+        }
+
+        static void InOrderWithoutRecursion1()
+        {
+            var node = root;
+            while (node != null || stack.Any())
+            {
+                TraverseLeftOnlyAndPushOnStack(node);
+                node = stack.Peek();
+                stack.Pop();
+                Console.WriteLine($"{node.Value}");
+                node = node.right;
+            }
         }
 
         static Node TraverseLeftOnlyAndPushOnStack(Node parentNode)
@@ -135,15 +237,24 @@ namespace Trees
             //root.left = new Node(11);
             //root.left.left = new Node(7);
             //root.left.left.right = new Node(6);
-            //root.left.left.right.left = new Node(8);
+            //root.left.left.right.left = new Node(18);
+            //root.left.left.right.right = new Node(19);
+            //root.left.left.right.left.right = new Node(25);
             //root.left.right = new Node(12);
+            //root.left.right.left = new Node(13);
+            //root.left.right.right = new Node(14);
             //root.right = new Node(9);
             //root.right.left = new Node(15);
             //root.right.right = new Node(8);
-
+            //PreOrder(root);
+            //PreOrderWithoutRecursion();
+            //PostOrder(root);
+            //Console.WriteLine("Without Recursion");
+            //PostOrderWithoutRecursion();
             //Console.WriteLine("Before Insert Inorder");
             //InOrder(root);
             //InOrderWithoutRecursion();
+            //InOrderWithoutRecursion1();
             //DifferentOrder(root);
             //Insert(55);
             //Console.WriteLine("After Insert Inorder");
@@ -155,6 +266,8 @@ namespace Trees
             //root.left = new Node(11);
             //root.left.left = new Node(7);
             //root.left.left.left = new Node(50);
+            //PostOrder(root);
+            //PostOrderWithoutRecursion();
             //InOrder(root);
             //InOrderWithoutRecursion();
             //DifferentOrder(root);
@@ -164,8 +277,10 @@ namespace Trees
             root.right = new Node(11);
             root.right.right = new Node(7);
             root.right.right.right = new Node(50);
-            InOrder(root);
-            InOrderWithoutRecursion();
+            PostOrder(root);
+            PostOrderWithoutRecursion();
+            //InOrder(root);
+            //InOrderWithoutRecursion();
             //DifferentOrder(root);
 
             Console.ReadLine();
