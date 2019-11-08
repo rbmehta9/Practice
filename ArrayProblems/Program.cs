555e1b7ae8c1b1baa215695a4fe7cb68411b7cd1
Ritesh Mehta <Ritesh.Mehta@csgi.com>
2019-11-08 14:21:24 -0600

    Binarybits implementation

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -54,16 +54,65 @@ namespace ArrayProblems
                 return "Printing " + str;
             return str;
         }
+    }
 
-        
+    /// <summary>
+    /// This is just to reimplement FindSubsets above to better understand as the idea is the same
+    /// </summary>
+    public static class BinaryBits
+    {
+        public static void PrintAllBinary(int numofDigits)
+        {
+            var subset = new int?[numofDigits];
+            PrintAllBinaryUtil(numofDigits, subset, 0);
+        }
 
+        public static void PrintAllBinaryUtil(int numofDigits, int?[] subset, int i)
+        {
+            if (i == numofDigits)
+            {
+                Console.WriteLine(PrintSubsets(subset));
+                return;
+            }
+
+            subset[i] = 0;
+            PrintAllBinaryUtil(numofDigits, subset, i + 1);
+
+            subset[i] = 1;
+            PrintAllBinaryUtil(numofDigits, subset, i + 1);
+        }
+
+        public static string PrintSubsets(int?[] subset, bool isPrinting = true)
+        {
+            var str = string.Empty;
+            for (var i = 0; i < subset.Length; i++)
+            {
+                if (subset[i].HasValue)
+                {
+                    if (i != subset.Length - 1)
+                        str += subset[i].Value + " , ";
+                    else
+                    {
+                        str += subset[i].Value;
+                    }
+                }
+
+            }
+
+            if (isPrinting)
+                return "Printing " + str;
+            return str;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            FindSubsets.FindAllSubsets(new []{0,1,2});
-            Console.ReadKey();
+            //FindSubsets.FindAllSubsets(new []{0,1,2});
+
+            BinaryBits.PrintAllBinary(5);
+
+            Console.ReadLine();
         }
     }
 }
